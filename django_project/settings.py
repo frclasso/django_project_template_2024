bcad53a7bd2b88f667b4eb167467d6ea593c9004
frclasso <frclasso@yahoo.com.br>
2024-10-10 16:04:24 -0300

    Adding profile pictures on home page

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # the place where the pictures will be saved
+MEDIA_URL = '/media/' # public picture url
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='/default/man_default.png', upload_to=f'profile_pics') # profile_pics/username/
+
+    def __str__(self):
+        return f'{self.user.username} profile.'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            outpusize = (300,300)
+            img.thumbnail(outpusize)
+            img.save(self.image.path)
