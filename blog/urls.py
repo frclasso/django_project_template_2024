6d7c246947f6bdc1540a84b9c9c1751296b17e48
frclasso <frclasso@yahoo.com.br>
2024-10-10 18:08:56 -0300

    Defined post create view method

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from . import views
+from .views import (
+    home, 
+    about, 
+    PostListView, 
+    PostDetailView, 
+    PostCreateView
+    )
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
-    path("about/", views.about, name="blog-about")
+    path("", PostListView.as_view(), name="blog-home"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("about/", about, name="blog-about")
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from .models import Post
-
-#dummy data
-# posts = [
-#      {
-#           'author': 'Fabio Classo',
-#           'title': 'Blog post 1',
-#           'content': 'First post content',
-#           'date_posted': datetime.now()
-#      },
-#      {
-#           'author': 'Jane Doe',
-#           'title': 'Blog post 2',
-#           'content': 'Second post content',
-#           'date_posted': datetime.now()
-#      }
-# ]
+from django.views.generic import (
+     ListView, 
+     DetailView, 
+     CreateView
+     )
 
 
 def home(request):
@@ -26,6 +15,23 @@ def home(request):
     }
     return render(request, 'blog/home.html', context=context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
       return render(request, 'blog/about.html', {'title':'About...'})
